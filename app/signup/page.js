a8029972752ceca816c582ca04fccff60d2049a5
suasha123 <suryaprataps471@gmail.com>
2025-09-02 23:18:27 +0530

    Added sign in sign up

---
+++ app/signup/page.js
@@ -0,0 +1,63 @@
+"use client";
+import { FcGoogle } from "react-icons/fc";
+const Signup = () => {
+  return (
+    <section className="w-full flex justify-center items-center min-h-screen bg-[#f6f6f6]">
+      <div className="w-[90%] sm:w-[400px] bg-white p-8 rounded-2xl shadow-xl">
+        <h1 className="text-[#3278e6] text-center text-2xl font-bold tracking-tight mb-5">
+          mockMate
+        </h1>
+
+        <h3 className="text-xl font-bold  text-center text-[#595959] mb-6">
+          Create Account
+        </h3>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="w-full px-4 py-2 border  rounded-xl focus:outline-none focus:border-none  border-grey-200 focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="w-full px-4 py-2 border rounded-xl focus:border-none  border-grey-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <button className="cursor-pointer w-full bg-blue-600 text-white py-2 rounded-xl font-medium hover:bg-blue-700 transition">
+          Sign Up
+        </button>
+
+        <div className="flex items-center my-6">
+          <hr className="flex-grow border-gray-300" />
+          <span className="px-2 text-sm text-gray-500">or</span>
+          <hr className="flex-grow border-gray-300" />
+        </div>
+
+        <button className="cursor-pointer w-full flex items-center justify-center gap-2 border py-2 rounded-xl hover:bg-gray-100 transition">
+          <FcGoogle className="w-6 h-6"/>
+          Continue with Google
+        </button>
+        
+        <p className="text-center text-sm text-gray-600 mt-6">
+          Already have an account?{" "}
+          <a href="/signin" className="text-blue-600 hover:underline">
+            Sign In
+          </a>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export const NavBar = () => {
+  const router = useRouter();
+  const gotSignIn = () => {
+    router.push("/signin");
+  };
   return (
     <nav className="w-full flex justify-center pt-0 sm:pt-8  sticky top-0 bg-[#f6f6f6] z-999">
       <div className="flex items-center justify-between w-[100%] sm:w-[90%] md:w-[85%] lg:w-[70%] px-3 sm:px-8 py-4 bg-white shadow-md  sm:rounded-2xl">
@@ -31,7 +39,10 @@ export const NavBar = () => {
             </a>
           </li>
         </ul>
-        <button className="cursor-pointer active:bg-[#3b82f6] px-5 py-2 rounded-xl bg-[#2b2c43] text-white text-[13px] md:text-[16px] shadow hover:bg-[#2563eb] transition-all duration-200 ease-in">
+        <button
+          className="cursor-pointer active:bg-[#3b82f6] px-5 py-2 rounded-xl bg-[#2b2c43] text-white text-[13px] md:text-[16px] shadow hover:bg-[#2563eb] transition-all duration-200 ease-in"
+          onClick={gotSignIn}
+        >
           Sign In
         </button>
       </div>
